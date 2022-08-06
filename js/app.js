e4a08296d91f2797c593b25be3f9fa67b8ca6efe
gile987 <mladen.reljic87@gmail.com>
2022-08-06 18:39:39 +0200

    Add a switch statement for different BMIs

--- js/app.js
+++ js/app.js
@@ -10,11 +10,36 @@ const calculate = () => {
 
   if (height === "" || isNaN(height)) {
     console.log("height is empty");
-    result.innerHTML = "Please enter a valid height";
+    result.innerHTML = "Please enter a valid number";
   } else if (weight === "" || isNaN(weight)) {
-    result.innerHTML = "Please enter a valid weight";
+    result.innerHTML = "Please enter a valid number";
   } else {
     let bmi = (weight / ((height * height) / 10000)).toFixed(2);
     result.innerHTML = "Your BMI is " + bmi;
+    switch (true) {
+      case bmi >= 6 && bmi < 16.0:
+        result.innerHTML += " and you are severely underweight";
+        break;
+      case bmi >= 16.0 && bmi < 18.5:
+        result.innerHTML += " and you are underweight";
+        break;
+      case bmi >= 18.5 && bmi <= 24.9:
+        result.innerHTML += " and your BMI is normal";
+        break;
+      case bmi >= 25 && bmi <= 29.9:
+        result.innerHTML += " and you are overweight";
+        break;
+      case bmi >= 30 && bmi <= 34.9:
+        result.innerHTML += " and you are obese";
+        break;
+      case bmi >= 35 && bmi <= 39.9:
+        result.innerHTML += " and you are severely obese";
+        break;
+      case bmi >= 40 && bmi <= 252:
+        result.innerHTML += " and you are morbidly obese";
+        break;
+      default:
+        result.innerHTML += " and you are not a human";
+    };
   };
 };
