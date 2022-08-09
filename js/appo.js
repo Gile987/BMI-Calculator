31e427f290e111b92fbd6d788c02f9a9b6e1d7fb
gile987 <mladen.reljic87@gmail.com>
2022-08-09 13:28:05 +0200

    Change the switch statement cases to cover all the ranges with shorter code

--- js/appo.js
+++ js/appo.js
@@ -26,31 +26,35 @@ class App {
 
   determineClass(bmi) {
     switch (true) {
-      case bmi >= 6 && bmi < 16.0:
+      case bmi < 6:
+        this.result.classList.add('not_human');
+        this.result.innerHTML += " and you are not a human";
+        break;
+      case bmi < 16.0:
         this.result.classList.add('severe');
         this.result.innerHTML += " and you are severely underweight";
         break;
-      case bmi >= 16.0 && bmi < 18.5:
+      case bmi <= 18.5:
         this.result.classList.add('underweight');
         this.result.innerHTML += " and you are underweight";
         break;
-      case bmi >= 18.5 && bmi <= 24.9:
+      case bmi <= 24.9:
         this.result.classList.add('normal');
         this.result.innerHTML += " and your BMI is normal";
         break;
-      case bmi >= 25 && bmi <= 29.9:
+      case bmi <= 29.9:
         this.result.classList.add('overweight');
         this.result.innerHTML += " and you are overweight";
         break;
-      case bmi >= 30 && bmi <= 34.9:
+      case bmi <= 34.9:
         this.result.classList.add('obese');
         this.result.innerHTML += " and you are obese";
         break;
-      case bmi >= 35 && bmi <= 39.9:
+      case bmi <= 39.9:
         this.result.classList.add('severely_obese');
         this.result.innerHTML += " and you are severely obese";
         break;
-      case bmi >= 40 && bmi <= 252:
+      case bmi <= 252:
         this.result.classList.add('morbidly_obese');
         this.result.innerHTML += " and you are morbidly obese";
         break;
