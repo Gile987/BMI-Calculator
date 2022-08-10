175dd472f3f17a635a27c36a5a5c93a3e0498456
gile987 <mladen.reljic87@gmail.com>
2022-08-10 13:45:35 +0200

    Write a script that checks whether the BMI number falls within a specific range, and then show a message depending on the range

--- js/appi.js
+++ js/appi.js
@@ -37,11 +37,42 @@ class DisplayResults {
   };
 };
 
+class BmiRange {
+  constructor(from, to, message, cssClass) {
+    this.from = from;
+    this.to = to;
+    this.message = message;
+    this.cssClass = cssClass;
+  };
+  checkRange(value) {
+    console.log(value)
+    if (value >= this.from && value <= this.to) {
+      return {message: this.message, cssClass: this.cssClass};
+    };
+  };
+};
+
+const bmiRanges = [
+  new BmiRange(0, 6, " and you are not a human", "not_human"),
+  new BmiRange(6, 16, " and you severely underweight", "severe"),
+  new BmiRange(16, 18.5, " and you underweight", "underweight"),
+  new BmiRange(18.5, 24.9, " and your BMI is normal", "normal"),
+  new BmiRange(25, 29.9, " and you are overweight", "overweight"),
+  new BmiRange(30, 34.9, " and you are obese", "obese"),
+  new BmiRange(35, 39.9, " and you are severely obese", "severely_obese"),
+  new BmiRange(40, 252, " and you are morbidly obese", "morbidly_obese"),
+  new BmiRange(253, 999, " and you are not a human", "not_human")
+];
+
 document.querySelector('.calculate').addEventListener('click', function() {
   userHeight = document.querySelector('.height').value;
   userWeight = document.querySelector('.weight').value;
+  result = document.querySelector('.results_text');
   let measurements = new Measurements();
   let bmi = new BMI(measurements);
   let displayResults = new DisplayResults(bmi);
+  let bmiRange = bmiRanges.find(bmiNum => bmiNum.checkRange(bmi.calculateBMI()));
   displayResults.displayResults();
+  result.classList.add(bmiRange.cssClass);
+  result.innerHTML += bmiRange.message;
 });
